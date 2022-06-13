cd6638788cf7c17355d987afba2c4687edf6f7f9
iubizi <106417012+iubizi@users.noreply.github.com>
2022-06-13 16:59:38 -0400

    Add files via upload

--- jfif2jpg.py
+++ jfif2jpg.py
@@ -13,8 +13,16 @@ for file in files:
         for i in name_list:
             name += i # 还原名字
             
-        name = name + '.jpg' # 转换为jpg
-        os.rename(file, name) # 改名
-        print(name) # 打印名称
+        file_name = name + '.jpg' # 转换为jpg
+
+        try:
+            os.rename(file, file_name) # 改名
+        except: # 出现重名问题
+            file_name = name + '.new.jpg' # 转换为jpg
+            os.rename(file, file_name) # 改名
+            
+        print(file_name) # 打印名称
+            
+
 
 input() # 看看结果
